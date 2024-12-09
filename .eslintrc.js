8bc9c68a5e92653a73740d6560120e2f12a85bf0
brucekang <radeonk@foxmail.com>
2024-12-09 22:23:04 +0800

    配置eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
-};
+  parser: '@typescript-eslint/parser', // 使用 TypeScript 解析器
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended', // 添加 TypeScript 推荐规则
+    'plugin:prettier/recommended'
+  ],
+  plugins: [
+    'react',
+    'react-native',
+    '@typescript-eslint' // 添加 TypeScript 插件
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2020, // 支持最新的 ECMAScript 版本
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect' // 自动检测所安装的 React 版本
+    }
+  },
+  rules: {
+    // 可以在这里自定义规则
+    'prettier/prettier': 'error'
+  }
+}
